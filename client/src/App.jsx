feca5da9c62c293697bc3e337e0151d6b7ce9197
Gustavo Inga <gustavoa_it@hotmail.com>
2024-11-18 23:53:15 -0500

    cambios en el front, agregando dependencias en front y back

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import {AuthProvider} from './context/AuthContext';
+import RegisterPage from './pages/RegisterPage';
+import LogInPage from './pages/LogInPage';
+import ProtectedRoute from './ProtectedRoute';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LogInPage />}/>
+          <Route path='/login' element={<LogInPage />}/>
+          <Route path='/register' element={<RegisterPage />}/>
+
+          <Route element={<ProtectedRoute/>}>
+            <Route path='/tasks' element={<h1>Tasks</h1>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+    
   )
 }
 
