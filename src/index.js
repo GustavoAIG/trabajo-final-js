ccbed29b501db887716cca70c09a634504c2cd53
Gustavo Inga <gustavoa_it@hotmail.com>
2024-12-01 17:24:33 -0500

    Agregando codigo adicional

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { server } from './app.js';  // Importa el servidor desde app.js
+import dotenv from 'dotenv';
+import { server } from './app.js'; // Importa el servidor desde app.js
 import { connectDB } from './db.js';
+
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
 
 connectDB();
 
 // Escucha en el puerto 4000
 server.listen(4000, () => {
-    console.log('Server on port', 4000);
+  console.log('Server on port 4000');
 });
