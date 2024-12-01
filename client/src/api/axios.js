f11c4204bc5a374583af43c265b0496a578f1a73
Gustavo Inga <gustavoa_it@hotmail.com>
2024-12-01 18:30:36 -0500

    cambios en el front deveritas

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://front-js.netlify.app/api',
+    baseURL: 'https://backend-mongo-in5c.onrender.com/api',
     withCredentials: true
 })
 
